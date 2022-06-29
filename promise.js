639244baddbd0103792bb7c4fc6c14b907369147
telus-tarun <tarun.sharma02@telusinternational.com>
2022-06-29 16:25:46 +0530

    removed extra files

--- promise.js
+++
@@ -1,7 +0,0 @@
-const myPromise = () => Promise.resolve('I have Resolved');
-
-function firstFunction() {
-  myPromise().then((res) => console.log(res));
-  console.log('second');
-}
-firstFunction()
